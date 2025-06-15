19a6641329477af69f1dc09a8621f5f48fbe9bae
agustin <agustin.piqueres@gmail.com>
2025-06-15 20:36:43 +0200

    Update config variables

--- src/dfcache/config.py
+++ src/dfcache/config.py
@@ -10,16 +10,16 @@ class Config:
     invalid_after: str | None = None
 
 
-cfg = Config()
+_cfg = Config()
 
 
 def set_config(**params: Mapping) -> None:
     """Configures global configuration."""
     import dfcache.config
 
-    valid_params = {k: v for k, v in params.items() if hasattr(dfcache.config.cfg, k)}
-    dfcache.config.cfg = replace(
-        dfcache.config.cfg,
+    valid_params = {k: v for k, v in params.items() if hasattr(dfcache.config._cfg, k)}
+    dfcache.config._cfg = replace(
+        dfcache.config._cfg,
         **valid_params,
     )
 
@@ -28,18 +28,18 @@ def get_config() -> Config:
     """Get the global config."""
     import dfcache.config
 
-    return dfcache.config.cfg
+    return dfcache.config._cfg
 
 
 def enable_caching():
     """Enable caching globally."""
     import dfcache.config
 
-    dfcache.config.cfg.caching_enabled = True
+    dfcache.config._cfg.caching_enabled = True
 
 
 def disable_caching():
     """Disable caching globally."""
     import dfcache.config
 
-    dfcache.config.cfg.caching_enabled = False
+    dfcache.config._cfg.caching_enabled = False
